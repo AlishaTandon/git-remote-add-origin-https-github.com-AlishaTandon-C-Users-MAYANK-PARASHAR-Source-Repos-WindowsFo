9f2db07fa74111f6934232aa723c75b2846c2e38
AlishaTandon <alisha.m.tandon@gmail.com>
2021-06-20 17:26:52 +0530

    Initial commit

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textRollNo = new System.Windows.Forms.TextBox();
+            this.textAadharNo = new System.Windows.Forms.TextBox();
+            this.textLastName = new System.Windows.Forms.TextBox();
+            this.textFirstName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Aqua;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(160, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(510, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Central Airmen Selection Board";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(299, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 34);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Data Search";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Navy;
+            this.button2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(741, 158);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 49);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textRollNo
+            // 
+            this.textRollNo.Font = new System.Drawing.Font("Arial Narrow", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textRollNo.Location = new System.Drawing.Point(454, 163);
+            this.textRollNo.Name = "textRollNo";
+            this.textRollNo.Size = new System.Drawing.Size(223, 44);
+            this.textRollNo.TabIndex = 9;
+            // 
+            // textAadharNo
+            // 
+            this.textAadharNo.Enabled = false;
+            this.textAadharNo.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textAadharNo.Location = new System.Drawing.Point(454, 360);
+            this.textAadharNo.Name = "textAadharNo";
+            this.textAadharNo.Size = new System.Drawing.Size(223, 44);
+            this.textAadharNo.TabIndex = 2;
+            // 
+            // textLastName
+            // 
+            this.textLastName.Enabled = false;
+            this.textLastName.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textLastName.Location = new System.Drawing.Point(454, 286);
+            this.textLastName.Name = "textLastName";
+            this.textLastName.Size = new System.Drawing.Size(223, 44);
+            this.textLastName.TabIndex = 1;
+            // 
+            // textFirstName
+            // 
+            this.textFirstName.Enabled = false;
+            this.textFirstName.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textFirstName.Location = new System.Drawing.Point(454, 226);
+            this.textFirstName.Name = "textFirstName";
+            this.textFirstName.Size = new System.Drawing.Size(223, 44);
+            this.textFirstName.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Gold;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(100, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 32);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "RollNo";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Gold;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(100, 238);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(157, 32);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "FirstName";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Gold;
+            this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(100, 298);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(154, 32);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "LastName";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Gold;
+            this.label7.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(100, 372);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(154, 32);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "AadharNo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Gold;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(100, 444);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 32);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "FingerPrint";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.DodgerBlue;
+            this.label9.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(736, 242);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 28);
+            this.label9.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(961, 539);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textFirstName);
+            this.Controls.Add(this.textLastName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textAadharNo);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textRollNo);
+            this.Name = "Form1";
+            this.Text = "CASB DATA SEARCH";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textRollNo;
+        private System.Windows.Forms.TextBox textAadharNo;
+        private System.Windows.Forms.TextBox textLastName;
+        private System.Windows.Forms.TextBox textFirstName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using OracleCommand = Oracle.ManagedDataAccess.Client.OracleCommand;
+using OracleConnection = Oracle.ManagedDataAccess.Client.OracleConnection;
+using OracleDataAdapter = Oracle.ManagedDataAccess.Client.OracleDataAdapter;
+
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String connectionstring = OracleConnString("mayank", "1521", "XE", "SYSTEM", "1234");
+            if (textRollNo.Text.Length != 10)
+            {
+                MessageBox.Show("Invalid Roll No");
+
+            }
+            else
+            {
+
+
+
+                OracleConnection con = new OracleConnection(connectionstring);
+                OracleCommand cmd = new OracleCommand();
+                cmd.CommandText = "Select * from CASBNEW.CASB_CANDIDATES where ROLL_NO=" + textRollNo.Text; ;
+                cmd.Connection = con;
+                OracleDataAdapter da = new OracleDataAdapter();
+                da.SelectCommand = cmd;
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    label9.Text = "Candidate Found Successfully";
+                    textFirstName.Text = (ds.Tables[0].Rows[0]["First_Name"].ToString()).Trim();
+                    textLastName.Text = (ds.Tables[0].Rows[0]["Last_Name"].ToString()).Trim();
+                    textAadharNo.Text = (ds.Tables[0].Rows[0]["Aadhar_No"].ToString()).Trim();
+                }
+                else
+                {
+                    label9.Text = "Candidate Not Found - Search Again";
+                    textRollNo.Text = "";
+                    textFirstName.Text = "";
+                    textLastName.Text = "";
+
+                }
+                con.Close();
+            }
+        }
+        public string OracleConnString(string host, string port, string servicename, string user, string pass)
+        {
+            return String.Format(
+              "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST={0})" +
+              "(PORT={1}))(CONNECT_DATA=(SERVICE_NAME={2})));User Id={3};Password={4};",
+              host,
+              port,
+              servicename,
+              user,
+              pass);
+        }
+
+    }
+}
+
+
+ 
